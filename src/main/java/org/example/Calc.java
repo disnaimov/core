8fb690aa9ae6b6c9464b04a37c751d86fac6a73f
disnaimov <dimitriy.kiselev0@mail.ru>
2024-07-06 15:36:15 +0400

    Task 1

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,10 @@
+package org.example;;
+
+class Calc {
+    public static void main(String[] args){
+        int a = Integer.valueOf(args[0]);
+        int b = Integer.valueOf(args[1]);
+
+        System.out.println(a + b);
+    }
+}
