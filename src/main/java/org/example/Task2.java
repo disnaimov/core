dc5d13d9150c515eccdf52921b2592cfd5a12f2c
disnaimov <dimitriy.kiselev0@mail.ru>
2024-07-09 19:35:00 +0400

    task 2 + task 3

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -1,4 +1,7 @@
 package org.example;
+
+import java.util.Scanner;
+
 /*
 Реализовать калькулятор вкладов - на вход приложение получает сумму вклада в рублях (может быть дробной, например, 1450.85 рублей),
 количество месяцев вклада (целое число) и годовой процент (может быть дробным, например, 14,5).
@@ -6,8 +9,28 @@ package org.example;
 Причем считаем, что капитализация вклада происходит каждый месяц - то есть, по истечении каждого месяца клиенту
 выплачиваются на счет проценты за прошедший месяц.
  */
-public class Main {
+public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Input sum: ");
+        double sum = scanner.nextDouble();
+
+        System.out.print("Input month: ");
+        int months = scanner.nextInt();
+
+        System.out.print("Input percent: ");
+        double percent = scanner.nextDouble();
+
+        double monthRate = percent / 12 / 100;
+
+        for (int i = 0; i < months; i++) {
+            double profit  = sum * monthRate;
+            sum += profit;
+        }
+
+        System.out.println("Sum is " + sum);
+
+        scanner.close();
     }
 }
---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,26 @@
+package org.example;
+/*
+Реализовать “домашний синглтон” - то есть класс - синглтон, который создается только в одном экземпляре.
+ */
+public class Task3 {
+
+   private static Task3 instance;
+
+   private Task3(){}
+
+    public static Task3 getInstance() {
+       if (instance == null) {
+           instance = new Task3();
+       }
+       return instance;
+    }
+
+    static class Singleton {
+        public static void main(String[] args) {
+            Task3 singleton1 =  Task3.getInstance();
+            Task3 singleton2 = Task3.getInstance();
+
+            System.out.println(singleton1 == singleton2);
+        }
+    }
+}
